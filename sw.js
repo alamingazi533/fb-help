06366becd5862e80ffb456a0e7ae35ce53c75b08
Md Alamin Islam <alamingazi533@gmail.com>
2026-08-22 10:43:49 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'fb-somadhan-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './profile.jpg',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.filter((name) => name !== CACHE_NAME)
+             .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // শুধু GET রিকোয়েস্ট ক্যাশ করা হয় (ফর্ম সাবমিট / POST রিকোয়েস্ট বাদ)
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return networkResponse;
+      }).catch(() => cached);
+
+      return cached || fetchPromise;
+    })
+  );
+});
